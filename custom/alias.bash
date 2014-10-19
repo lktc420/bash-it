4c70bb233ef8032d00dd4b8edf8eba5d8782b20b
zanmato1984 <zanmato1984@gmail.com>
2014-10-20 00:39:06 +0800

    Adding alias and env into bash-it.

---
+++ custom/alias.bash
@@ -0,0 +1,49 @@
+# Scripts
+alias sudo="sudo -E "
+alias fp="$HOME_ABS/script/fp.sh"
+alias clipfp="$HOME_ABS/script/clipfp.sh"
+alias selfp="$HOME_ABS/script/selfp.sh"
+alias pk="$HOME_ABS/script/pk.sh"
+alias jpk="$HOME_ABS/script/jpk.sh"
+
+alias smst="$HOME_ABS/script/start_metastore.sh"
+alias sall="$HOME_ABS/script/start_all.sh"
+
+alias insthv="$HOME_ABS/script/install_hive.sh"
+alias instspk="$HOME_ABS/script/install_spark.sh"
+alias instshk="$HOME_ABS/script/install_shark.sh"
+alias scpinst="$HOME_ABS/script/scp_install.sh"
+
+alias updtrp="$HOME_ABS/script/update_repo.sh"
+
+# Locations
+alias gdev="cd $DEVROOT"
+alias ghv="cd $DEVROOT/$HIVEROOT/src"
+alias gngmr="cd $DEVROOT/ngmr-1.7-transwarp"
+
+# Commands
+alias tls="tmux ls" 
+alias tat="tmux a -t" 
+alias tns="tmux new-session -s"
+
+alias msql="mysql -uroot -pwarp123"
+alias trswp="transwarp -N"
+alias hbase="/usr/lib/hbase/bin/hbase shell"
+
+alias thive="$HOME_ABS/script/hive.tmux.sh"
+
+alias rbdngmr="cd spark; sbt/sbt clean; cd ..; cd shark; sbt/sbt clean; cd ..; ./compile.sh init; ./compile.sh spark; ./compile.sh shark"
+
+if [ $(uname) = "Darwin" ]; then
+    alias clipcpy="tr -d '\n' | pbcopy"
+    alias clippst="pbpaste"
+    alias selcpy="tr -d '\n' | pbcopy"
+    alias selpst="pbpaste"
+else if [ $(uname) = "Linux" ]; then
+    alias clipcpy="tr -d '\n' | xsel -b"
+    alias clippst="xsel -b -o"
+    alias selcpy="tr -d '\n' | xsel"
+    alias selpst="xsel -o"
+fi
+
+
---
+++ custom/env.bash
@@ -0,0 +1,24 @@
+if [ $(uname) = "Darwin" ]; then
+    export HOME_ABS="/Users/zanmato"
+else if [ $(uname) = "Linux" ]; then
+    export HOME_ABS="/home/ruoxi"
+fi
+if [ $(uname) = "Darwin" ]; then
+    export DEVROOT=$HOME_ABS/git
+else if [ $(uname) = "Linux" ]; then
+    export DEVROOT=$HOME_ABS/dev
+fi
+export JAVA_HOME=/usr/lib/jvm/java-6-oracle
+export SCALA_HOME=/usr/lib/scala-2.11.1
+export PATH=$JAVA_HOME:$JAVA_HOME/bin:$SCALA_HOME/bin:$HIVE_HOME/bin:/usr/lib/hadoop/bin:/usr/lib/hadoop-hdfs/bin:$PATH
+export HADOOP_HOME=/usr/lib/hadoop
+export HADOOP_LIBEXEC_DIR=$HADOOP_HOME/libexec
+export HIVE_HOME=/usr/lib/hive
+export HIVEROOT=hive-0.12.0-transwarp
+#export HIVEROOT=WARP-1019-hive-0.12.0-transwarp
+export HBASE_CONF_DIR=/usr/lib/hbase/conf
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+export POOIQWRKNV_JKLJFWPEO_DFQQWE_LBIERPL_MDCJKD_NOT_RECOMMANDED=true
+
+export SVN_HIVE_BRANCH=https://172.16.1.168/transwarp/sbranches/ruoxi/warp-1336-hive-0.12.0-transwarp/hive-0.12.0-transwarp
+export SVN_HIVE_TRUNK=https://172.16.1.168/transwarp/trunk/hadoop/sources/hive-0.12.0-transwarp
