b3841a46e49695fcbce2936b0ed59de02a059815
zanmato1984 <zanmato1984@gmail.com>
2014-10-24 01:03:40 +0800

    Refine env.

--- custom/env.bash
+++ custom/env.bash
@@ -1,14 +1,12 @@
 if [ $(uname) = "Darwin" ]; then
     export HOME_ABS=/Users/zanmato
+    export DEVROOT=$HOME_ABS/git
     export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 elif [ $(uname) = "Linux" ]; then
     export HOME_ABS=/home/ruoxi
-    export JAVA_HOME=/usr/lib/jvm/java-6-oracle
-fi
-if [ $(uname) = "Darwin" ]; then
-    export DEVROOT=$HOME_ABS/git
-elif [ $(uname) = "Linux" ]; then
     export DEVROOT=$HOME_ABS/dev
+    export JAVA_HOME=/usr/lib/jvm/java-6-oracle
+    export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 fi
 export SCRIPT_HOME=$BASH_IT/script
 export CONF_HOME=$BASH_IT/conf
@@ -20,7 +18,6 @@ export HIVE_HOME=/usr/lib/hive
 export HIVEROOT=hive-0.12.0-transwarp
 #export HIVEROOT=WARP-1019-hive-0.12.0-transwarp
 export HBASE_CONF_DIR=/usr/lib/hbase/conf
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 export POOIQWRKNV_JKLJFWPEO_DFQQWE_LBIERPL_MDCJKD_NOT_RECOMMANDED=true
 
 export SVN_HIVE_BRANCH=https://172.16.1.168/transwarp/sbranches/ruoxi/warp-1336-hive-0.12.0-transwarp/hive-0.12.0-transwarp
