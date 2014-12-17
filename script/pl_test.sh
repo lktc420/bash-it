e5dd0ba61b1053d16b515e3804dcf4afc1d71ef4
ruoxi <zanmato1984@gmail.com>
2014-12-17 11:06:17 +0800

    Update pl test.

--- script/pl_test.sh
+++ script/pl_test.sh
@@ -2,14 +2,37 @@
 
 PL_TEST_ROOT=$DEVROOT/$HIVEROOT/src/sqlUnitTest/pl
 
-for sql in `find $PL_TEST_ROOT -name '*.sql'` ; do
-  log=${sql/\.sql/\.log}
-  echo "Running $sql ..."
-  $SCRIPT_HOME/start_clidriver.sh -f $sql >& $log 
+TEST_DIRS=(
+anon_exec
+collection
+conversion
+cursor
+empty_func
+for_all
+for_loop
+goto
+query
+select_into
+simple_loop
+syscall
+type
+udf
+while_loop
+)
+
+for dir in ${TEST_DIRS[@]}; do
+  for ref in `find $PL_TEST_ROOT/$dir -name '*.txt'` ; do
+    sql=${ref/\.txt/\.sql}
+    log=${ref/\.txt/\.log}
+    echo "Running $sql ..."
+    $SCRIPT_HOME/start_clidriver.sh -f $sql | grep -v "^OK" | grep -v "Time taken:" | grep -v "output" >& $log 
+  done
 done
 
-for ref in `find $PL_TEST_ROOT -name '*.txt'` ; do
-  log=${ref/\.txt/\.log}
-  echo "Diffing $log ..."
-  vimdiff $ref $log
+for dir in ${TEST_DIRS[@]}; do
+  for ref in `find $PL_TEST_ROOT/$dir -name '*.txt'` ; do
+    log=${ref/\.txt/\.log}
+    echo "Diffing $log ..."
+    vimdiff $ref $log
+  done
 done
